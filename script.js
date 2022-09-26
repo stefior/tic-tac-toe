8bc422721528efd216de7b1fc01ba46838e7a31c
Stefior <stephencfiorenza@gmail.com>
2022-09-26 19:47:22 -0400

    Add functionality for showing board contents

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 "use strict";
 
+const gameBoard = (() => {
+  const x = 'X';
+  const o = 'O';
+  const board = [x, o, x,
+                 o, x, o,
+                 o, o, x];
+  function displayBoard() {
+    let container = document.querySelector('div.container');
+    for (let square of board) {
+      const mark = document.createElement('div');
+      mark.textContent = square;
+      container.appendChild(mark);
+    }
+  }
+  return {displayBoard};
+})();
+
+(function flowControl() {
+  
+  return {};
+})();
+
+function Player() {
+  
+  return {};
+}
